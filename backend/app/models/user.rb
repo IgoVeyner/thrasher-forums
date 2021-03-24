7ca2e6d51dc65ce76339cc2a3b09d9a2820d31e8
Igor Veyner <igorveyner@gmail.com>
2021-03-24 17:39:42 -0400

    fix user validation typo

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates_uniqueness_of: username
-  validates_uniqueness_of: email
+  validates_uniqueness_of :username;
+  validates_uniqueness_of :email;
 end
