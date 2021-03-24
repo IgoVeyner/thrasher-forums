5beee653de332f80b2e51cd2f50ff8f55816f46d
Igor Veyner <igorveyner@gmail.com>
2021-03-24 17:36:20 -0400

    intial commit

---
+++ backend/app/controllers/users_controller.rb
@@ -0,0 +1,17 @@
+class UsersController < ApplicationController
+  def create
+    user = User.new(user_params)
+
+    if user.save 
+      render json: UserSerializer.new(user)
+    else 
+      # render error
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password, :email)
+  end
+end
