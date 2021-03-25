412163f7b6c0c7ded94c77c2d63265e7b24beea3
Igor Veyner <igorveyner@gmail.com>
2021-03-25 15:26:41 -0400

    add redux and combine reducer to frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,10 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import rootReducer from './redux/reducers';
+
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+const store = createStore(rootReducer);
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store} >
     <App />
-  </React.StrictMode>,
+  </Provider >,
   document.getElementById('root')
 );
 
