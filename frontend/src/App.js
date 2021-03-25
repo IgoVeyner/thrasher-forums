ceea4be2bf97f71bdbff1e69c2e8d58598b93163
Igor Veyner <igorveyner@gmail.com>
2021-03-25 15:50:24 -0400

    add Router to frontend and reorganize dom structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,15 @@
-import './App.css';
+import './App.css'
+import Home from './redux/components/home'
 import SignupForm from './redux/components/forms/signup'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 function App() {
   return (
-    // <div>temp</div>
-    <SignupForm />
+    <Router>
+      <Route path="/" component={Home} />
+      <Route path="/signup" exact component={SignupForm} />
+
+    </Router>
   );
 }
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
 
-import rootReducer from './redux/reducers';
+import rootReducer from './redux/reducers'
 
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
 
 const store = createStore(rootReducer);
 
