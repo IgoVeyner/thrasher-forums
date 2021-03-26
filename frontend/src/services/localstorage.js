4dde4b6cbb4ae6fa09b5d9e5e62bd4eab006c73d
Igor Veyner <igorveyner@gmail.com>
2021-03-26 19:14:52 -0400

    add localstorage file to handle localstorage tasks

---
+++ frontend/src/services/localstorage.js
@@ -0,0 +1,3 @@
+export const getToken = () => localStorage.getItem('jwt')
+export const setToken = token => localStorage.setItem('jwt')
+export const clearToken = () => localStorage.removeItem('jwt')
