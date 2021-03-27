4ba1911822a7a44e8a9056fb783fffece48fde7a
Igor Veyner <igorveyner@gmail.com>
2021-03-27 16:33:03 -0400

    stub out useEffect for BoardsContainer

--- frontend/src/redux/containers/boardsContainer.js
+++ frontend/src/redux/containers/boardsContainer.js
@@ -1,9 +1,16 @@
+import { useEffect } from 'react'
+
 export default function BoardsContainer() {
+
+  useEffect(() => {
+    // use api to fetch for boards
+  }, []);
+
   return (
     <div>
       <h1>Boards</h1>
       <ul>
-
+        
       </ul>
     </div>
   )
