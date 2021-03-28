17454060e897f5a57c44622bbc29787f631d7070
Igor Veyner <igorveyner95@gmail.com>
2021-03-28 17:19:53 -0400

    add postReducer and add to the root reducer

--- frontend/src/redux/reducers/index.js
+++ frontend/src/redux/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from "redux"
 import handleUsers from './userReducer'
 import handleBoards from './boardReducer'
 import handleBusy from './busyReducer';
+import handlePosts from './postReducer'
 
 const rootReducer = combineReducers({
   busy: handleBusy,
   user: handleUsers,
-  boards: handleBoards
+  boards: handleBoards,
+  posts: handlePosts
 })
 
 export default rootReducer
---
+++ frontend/src/redux/reducers/postReducer.js
@@ -0,0 +1,17 @@
+function handlePosts(state = [], action) {
+  switch (action.type) {
+    case "SET_POSTS":
+      return action.payload
+
+    case "ADD_POST":
+      return [...state, action.post]  
+
+    case "REMOVE_POST":
+      return state.filter(post => post.id !== action.payload)
+
+    default:
+      return state
+  }
+}
+
+export default handlePosts
