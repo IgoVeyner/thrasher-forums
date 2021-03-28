e7bc7e0a33d5aab9a921826217fc9f1a8d385864
Igor Veyner <igorveyner95@gmail.com>
2021-03-28 17:41:56 -0400

    clean up and add todo message

--- frontend/src/redux/actions/postActions.js
+++ frontend/src/redux/actions/postActions.js
@@ -1,10 +1,11 @@
 import { fetchPosts } from '../../services/api'
 
 export const getPosts = boardName => {
-  return (dispatch) => {
-    console.log(boardName)
+  return dispatch => {
     dispatch({ type: "FETCHING" })
     fetchPosts(boardName).then(response => {
+      // todo: 
+      // after adding posts to backend and updating serializer, send posts in dispatch below
       console.log(response)
       // dispatch({type: "SET_POSTS", payload: response})
     })
