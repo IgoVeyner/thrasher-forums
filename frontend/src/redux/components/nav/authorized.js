3b1284df1ea7ce8c3e3f2fdbcd4f91ddb07054d0
Igor Veyner <igorveyner95@gmail.com>
2021-03-28 16:58:45 -0400

    fix nav component to render in correct order

--- frontend/src/redux/components/nav/authorized.js
+++ frontend/src/redux/components/nav/authorized.js
@@ -4,8 +4,8 @@ export default function Authorized({ handleLogout }) {
   return (
     <>
       <Link to="/#">Events</Link>
-      <Link to="/#">Photos</Link>
       <Link to="/#">Videos</Link>
+      <Link to="/#">Photos</Link>
       <button onClick={handleLogout}>LOGOUT</button>
     </>
   )
