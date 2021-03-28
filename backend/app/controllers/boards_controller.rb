bcf2b25e9641f455e46396a0fe1cb5915624bd83
Igor Veyner <igorveyner95@gmail.com>
2021-03-28 15:14:24 -0400

    add show index, teach board how to slug their name and how to find by slug

--- backend/app/controllers/boards_controller.rb
+++ backend/app/controllers/boards_controller.rb
@@ -4,4 +4,14 @@ class BoardsController < ApplicationController
     seralized_boards = boards.map {|board| BoardSerializer.new(board)}
     render json: {boards: seralized_boards}, status: :accepted
   end
+
+  def show 
+    board = Board.find_by_slug(params[:id])
+    if board 
+      render json: {board: BoardSerializer.new(baord)}, status: :accepted
+    else 
+      render json: {error: "No board found"}, status: :not_acceptable
+    end
+  end
+  
 end
--- backend/app/models/board.rb
+++ backend/app/models/board.rb
@@ -1,3 +1,11 @@
 class Board < ApplicationRecord
+  def slug 
+    self.name.gsub(" ", "-")
+  end
+
+  def self.find_by_slug(slug)
+    self.all.find {|game| game.slug == slug}
+  end
+
   validates :name, uniqueness: true
 end
