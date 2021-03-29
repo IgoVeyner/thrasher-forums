b7ca3ab7ba64eeae86270c854cebedaf7c4530f0
Igor Veyner <igorveyner95@gmail.com>
2021-03-29 15:26:20 -0400

    render date and time post was created

--- frontend/src/redux/components/post.js
+++ frontend/src/redux/components/post.js
@@ -2,11 +2,18 @@ import { Link } from 'react-router-dom'
 
 export default function Post({ post }) {
 
+  const renderCreatedAt = () => {
+    const split = post.created_at.split('T')
+    const time = split[1].split('.')[0]
+    return <p>Created on: {split[0]} at: {time}</p>
+  }
+
   return (
     // add link to post show page
     // <Link to={"/"} >
       <div>
         <h2>{post.title}</h2>
+        {renderCreatedAt()}
       </div>
     // </Link>
   )
