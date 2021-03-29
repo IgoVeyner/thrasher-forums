d44f2a1de07c33024b32e301f25ca06d4bbf5537
Igor Veyner <igorveyner95@gmail.com>
2021-03-29 14:51:58 -0400

    add text column to posts table

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_29_173222) do
+ActiveRecord::Schema.define(version: 2021_03_29_185106) do
 
   create_table "boards", force: :cascade do |t|
     t.string "name"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2021_03_29_173222) do
     t.integer "board_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "text"
   end
 
   create_table "users", force: :cascade do |t|
