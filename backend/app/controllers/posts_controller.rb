5751edd691b9d375761af320409bec3214e4d624
Igor Veyner <igorveyner95@gmail.com>
2021-03-30 14:27:00 -0400

    finish create action

--- backend/app/controllers/posts_controller.rb
+++ backend/app/controllers/posts_controller.rb
@@ -1,12 +1,25 @@
 class PostsController < ApplicationController
   
   def create
-    # create a post and return it or return an error
+    user = User.find_by(username: post_params[:username])
+    board = Board.find_by(name: post_params[:board_name])
+    post = Post.create(
+      title: post_params[:title], 
+      text: post_params[:text],
+      user_id: user.id,
+      board_id: board.id
+    )
+
+    if post 
+      render json: { post: PostSerializer.new(post) }, status: :created
+    else
+      render json: { error: 'failed to create post', messages: post.errors.full_messages }, status: :not_acceptable
+    end
   end
 
   private
 
   def post_params
-    params.require(:post).permit(:title, :text, :board_id, :user_id)
+    params.require(:post).permit(:title, :text, :board_name, :username)
   end
 end
