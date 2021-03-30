acf20dd268f7912c7da73985bc3d2b8530cf0e87
Igor Veyner <igorveyner95@gmail.com>
2021-03-30 13:59:47 -0400

    stub out create action and private post_params helper

--- backend/app/controllers/posts_controller.rb
+++ backend/app/controllers/posts_controller.rb
@@ -1,2 +1,12 @@
 class PostsController < ApplicationController
+  
+  def create
+    # create a post and return it or return an error
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text, :board_id, :user_id)
+  end
 end
