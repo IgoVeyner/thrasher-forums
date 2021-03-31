7c7215c6714b20df719a348f57e8d0be987f7db7
Igor Veyner <igorveyner95@gmail.com>
2021-03-31 16:43:26 -0400

    create comments create action and private comment params

--- backend/app/controllers/comments_controller.rb
+++ backend/app/controllers/comments_controller.rb
@@ -1,2 +1,24 @@
 class CommentsController < ApplicationController
+  
+  def create 
+    user = User.find_by(username: comment_params[:username])
+    comment = Comment.create(
+      text: comment_params[:text],
+      user_id: user.id,
+      post_id: comment_params[:post_id]
+    )
+    
+    if comment.valid?
+      render json: { comment: CommentSerializer.new(comment) }, status: :created 
+    else
+      render json: { error: 'failed to create comment', messages: comment.errors.full_messages }, status: :not_acceptable
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text, :username, :post_id)
+  end
+
 end
--- frontend/src/redux/actions/commentActions.js
+++ frontend/src/redux/actions/commentActions.js
@@ -1,5 +1,3 @@
-import { postComment } from '../../services/api'
-
 export const addComment = comment => {
   return dispatch => {
     dispatch({ type: "ADD_COMMENT", payload: comment })
