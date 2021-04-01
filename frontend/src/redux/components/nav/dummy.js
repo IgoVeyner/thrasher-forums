79b3bc39df5aa80449b42f8363c8bd2cace5ac0b
Igor Veyner <igorveyner95@gmail.com>
2021-04-01 15:08:04 -0400

    finish dummy icons

--- frontend/src/redux/components/nav/dummy.js
+++ frontend/src/redux/components/nav/dummy.js
@@ -1,11 +1,11 @@
 export default function Dummy() {
   return (
     <nav className="dummy-nav" >
-      <span className="dummy" id="mail">mail</span>
-      <span className="dummy" id="facebook">facebook</span>
-      <span className="dummy" id="instagram">instagram</span>
-      <span className="dummy" id="twitter">twitter</span>
-      <span className="dummy" id="youtube">youtube</span>
+      <span className="dummy" id="mail">m</span>
+      <span className="dummy" id="facebook">f</span>
+      <span className="dummy" id="instagram">i</span>
+      <span className="dummy" id="twitter">t</span>
+      <span className="dummy" id="youtube">y</span>
       {/* dummy searchbar */}
     </nav>
   )
