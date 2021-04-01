ec6d2bb569ded1f24ba9b90d2de4b304a716f15e
Igor Veyner <igorveyner95@gmail.com>
2021-04-01 11:37:57 -0400

    move css files to styles folder and update files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import './App.css'
+import './styles/App.css'
 import Home from './redux/components/home'
 import SignupForm from './redux/components/forms/signup'
 import LoginForm from './redux/components/forms/login'
