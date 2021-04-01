922d9bd10d8e5f7f78de314c0bf27662f3cb0a42
Igor Veyner <igorveyner95@gmail.com>
2021-04-01 14:34:36 -0400

    add dummy component

---
+++ frontend/src/redux/components/nav/dummy.js
@@ -0,0 +1,12 @@
+export default function Dummy() {
+  return (
+    <nav className="dummy-nav" >
+      <span className="dummy">mail</span>
+      <span className="dummy">facebook</span>
+      <span className="dummy">instagram</span>
+      <span className="dummy">twitter</span>
+      <span className="dummy">youtube</span>
+      {/* dummy searchbar */}
+    </nav>
+  )
+}
