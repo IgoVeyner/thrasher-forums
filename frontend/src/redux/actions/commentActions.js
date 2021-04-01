76635eb8457e44620743656204c82d208150ab9c
Igor Veyner <igorveyner95@gmail.com>
2021-04-01 10:27:44 -0400

    add removeComment action

--- frontend/src/redux/actions/commentActions.js
+++ frontend/src/redux/actions/commentActions.js
@@ -1,5 +1,17 @@
+import { destroyComment } from '../../services/api'
+
 export const addComment = comment => {
   return dispatch => {
     dispatch({ type: "ADD_COMMENT", payload: comment })
+  } 
+}
+
+export const removeComment = id => {
+  return dispatch => {
+    dispatch({ type: "FETCHING" })
+    destroyComment(id)
+    .then(response => {
+      dispatch({ type: "REMOVE_COMMENT", payload: id})
+    })
   }
 }
