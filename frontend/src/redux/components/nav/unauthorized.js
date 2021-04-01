fa3f0bd2adb9263aad75ef32eab19cd249d82797
Igor Veyner <igorveyner95@gmail.com>
2021-04-01 12:07:20 -0400

    nav styling

--- frontend/src/redux/components/nav/unauthorized.js
+++ frontend/src/redux/components/nav/unauthorized.js
@@ -3,8 +3,16 @@ import { Link } from 'react-router-dom'
 export default function Unauthorized() {
   return (
     <nav className="nav" >
-      <Link to="/login">Login</Link>
-      <Link to="/signup">signup</Link>
+      <Link to="/login" className="nav-link" >
+        <span className="thrasher" >
+          LOGIN
+        </span>
+      </Link>
+      <Link to="/signup" className="nav-link" >
+        <span className="thrasher" >
+          SIGNUP
+        </span>
+      </Link>
     </nav>
   )
 }
