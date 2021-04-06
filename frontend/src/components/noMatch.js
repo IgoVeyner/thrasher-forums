f7c70ca98657c06bcdce7311d64ac9eb61929762
Igor Veyner <igorveyner95@gmail.com>
2021-04-06 08:48:57 -0400

    style 404 component

--- frontend/src/components/noMatch.js
+++ frontend/src/components/noMatch.js
@@ -1,9 +1,9 @@
 export default function NoMatch() {
 
   return (
-    <div>
-      <h1>Error 404</h1>
-      <h2>Path not found</h2>
+    <div id='error-404'>
+      <h1 className="thrasher">Error 404</h1>
+      <h2 className="thrasher">Path not found</h2>
     </div>
   )
 }
