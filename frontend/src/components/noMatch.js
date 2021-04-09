c951f3233cab7b040a7a88d363e19795226485b0
Igor Veyner <igorveyner95@gmail.com>
2021-04-09 11:21:49 -0400

    style error page container and background

--- frontend/src/components/noMatch.js
+++ frontend/src/components/noMatch.js
@@ -1,9 +1,12 @@
 export default function NoMatch() {
 
   return (
-    <div id='error-404'>
-      <h1 className="thrasher">Error 404</h1>
-      <h2 className="thrasher">Path not found</h2>
+    <div id="error-container">
+      <div id='error-404'>
+        
+        <h1 className="thrasher">Error 404</h1>
+        <h2 className="thrasher">Path not found</h2>
+      </div>
     </div>
   )
 }
