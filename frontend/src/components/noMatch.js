202f6265e8233b2e1e3575ad0fddaa475a4f96dd
Igor Veyner <igorveyner95@gmail.com>
2021-04-09 11:25:15 -0400

    style error page header and text

--- frontend/src/components/noMatch.js
+++ frontend/src/components/noMatch.js
@@ -4,7 +4,10 @@ export default function NoMatch() {
     <div id="error-container">
       <div id='error-404'>
         
-        <h1 className="thrasher">Error 404</h1>
+        <div id="error-header-container">
+          <h1 className="thrasher" id="error-header">Error 404</h1>
+        </div>
+
         <h2 className="thrasher">Path not found</h2>
       </div>
     </div>
