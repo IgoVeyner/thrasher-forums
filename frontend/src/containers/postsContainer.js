9b57b4d1e2b94b5cf3f676b99c20c913d3e1dd30
Igor Veyner <igorveyner95@gmail.com>
2021-04-12 16:03:53 -0400

    fix spacing

--- frontend/src/containers/postsContainer.js
+++ frontend/src/containers/postsContainer.js
@@ -15,7 +15,6 @@ export default function PostContainer() {
   const posts = useSelector(state => state.posts)
   const user = useSelector(state => state.user)
   const dispatch = useDispatch()
-
   const submitPost = post => dispatch(sendPost(post))
   const deletePost = id => dispatch(removePost(id)) 
 
