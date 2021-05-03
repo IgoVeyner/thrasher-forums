4f15d18e3244c6b050e21b3667c41d3cd8e8d60d
Igor Veyner <igorveyner95@gmail.com>
2021-05-03 17:24:23 -0400

    reorder element order for posts

--- frontend/src/components/post.js
+++ frontend/src/components/post.js
@@ -32,11 +32,11 @@ export default function Post({ post, currentUser, deletePost }) {
     <Link to={`/posts/${id}`} className="post-link" >
       <div className="post-text">
         <h2 className="post-title">{title}</h2>
-        {currentUser === user ? renderDeleteButton() : null}
+        {renderCreatedAt()}
         <p className="post-description">u: 
           <span className="bold"> {user}</span>
         </p>
-        {renderCreatedAt()}
+        {currentUser === user ? renderDeleteButton() : null}
       </div>
     </Link>
   )
