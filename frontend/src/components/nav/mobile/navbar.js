ed73a287c2a1dac9a0cb99e639f9e570ccd46975
Igor Veyner <igorveyner95@gmail.com>
2021-05-03 16:15:41 -0400

    create template mobile navbar

---
+++ frontend/src/components/nav/mobile/navbar.js
@@ -0,0 +1,21 @@
+import Logo from '../logo'
+import { useSelector } from 'react-redux'
+
+// TODO:
+// center Logo
+// create dropdown button
+// import dropdown button
+// render dropdown button
+
+export default function MobileNavBar({ handleLogout }) {
+
+  const user = useSelector(state => state.user)
+
+  return (
+    <header>
+      <Logo />
+      
+      {/* Render a dropdown button */}
+    </header>
+  )
+}
