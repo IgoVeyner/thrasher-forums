ba82bb964d093fd9e280916e5f4cb4b308825dc4
Igor Veyner <igorveyner95@gmail.com>
2021-05-17 15:32:29 -0400

    create new dummy mobile nav

---
+++ frontend/src/components/nav/mobile/dummy.js
@@ -0,0 +1,11 @@
+export default function Dummy() {
+  return (
+    <nav id="dummy-mobile-nav" >
+      <span className="dummy" id="mail">m</span>
+      <span className="dummy" id="facebook">f</span>
+      <span className="dummy" id="instagram">i</span>
+      <span className="dummy" id="twitter">t</span>
+      <span className="dummy" id="youtube">y</span>
+    </nav>
+  )
+}
