33d4a1757d9144d34e77f7b21d0d341e1b5a228f
Igor Veyner <igorveyner95@gmail.com>
2021-05-21 07:50:08 -0400

    start refactor of unauthorized nav component

--- frontend/src/components/nav/navbar.js
+++ frontend/src/components/nav/navbar.js
@@ -8,6 +8,7 @@ export default function NavBar({ handleLogout }) {
 
   const user = useSelector(state => state.user)
   const classArgs = ["nav", "nav-link", "logout", "logout-text"]
+  const unAuthClassArgs = ["nav", "nav-link"]
 
   return (
     <header id="nav">
@@ -19,7 +20,7 @@ export default function NavBar({ handleLogout }) {
         { user !== "" ? 
           <Authorized handleLogout={handleLogout} classArgs={classArgs}/> 
           : 
-          <Unauthorized /> 
+          <Unauthorized classArgs={unAuthClassArgs} /> 
         }
       </div>
     </header>
--- frontend/src/components/nav/unauthorized.js
+++ frontend/src/components/nav/unauthorized.js
@@ -1,16 +1,19 @@
 import { Link } from 'react-router-dom' 
 
-export default function Unauthorized() {
+export default function Unauthorized({ resetDropDown = null, classArgs }) {
+  
+  const [navClassName, linkClassName] = classArgs
+
   return (
 
     <nav className="nav" >
-      <Link to="/login" className="nav-link" >
+      <Link to="/login" className="nav-link" onClick={resetDropDown} >
         <span className="thrasher" >
           LOGIN
         </span>
       </Link>
     
-      <Link to="/signup" className="nav-link" >
+      <Link to="/signup" className="nav-link" onClick={resetDropDown} >
         <span className="thrasher" >
           SIGNUP
         </span>
