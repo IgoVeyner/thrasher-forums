57cdf3a97c22f2485fe804a89afbd906a35f8159
Igor Veyner <igorveyner95@gmail.com>
2021-05-21 07:45:39 -0400

    render mobile auth component using new refactored component

--- frontend/src/components/nav/mobile/dropdown.js
+++ frontend/src/components/nav/mobile/dropdown.js
@@ -1,14 +1,16 @@
-import Authorized from './authorized'
+// import Authorized from './authorized'
+import Authorized from '../authorized'
 import Unauthorized from './unauthorized'
 import DummyNav from './dummy'
 import DummyForm from '../../forms/mobileDummyForm'
 
 export default function DropDown({ user, handleLogout, resetDropDown }) {
+  const classArgs = ["mobile-nav-link-container", "mobile-nav-link", "mobile-logout", "mobile-logout-text"]
 
   return (
     <div id="mobile-dropdown">
       { user !== "" ? 
-        <Authorized handleLogout={handleLogout} resetDropDown={resetDropDown}/> 
+        <Authorized handleLogout={handleLogout} resetDropDown={resetDropDown} classArgs={classArgs} /> 
         : 
         <Unauthorized resetDropDown={resetDropDown}/> 
       }
